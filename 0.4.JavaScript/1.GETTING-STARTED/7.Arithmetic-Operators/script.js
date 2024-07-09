77199d89c107d7fb82930ce4b5b75d49ee92d866
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-09 17:54:50 +0530

    MERN COURSE : Java Script 8

---
+++ 0.4.JavaScript/1.GETTING-STARTED/7.Arithmetic-Operators/script.js
@@ -0,0 +1,50 @@
+// ? Basic Arithmetic Operations
+
+let a = 5;
+let b = 2;
+
+// ! Addition
+
+const results = a + b;
+console.log("Addition: ", results);
+
+// ! Subtraction
+
+const results2 = a - b;
+console.log("Subtraction: ", results2);
+
+//multiplication
+
+const results3 = a * b;
+console.log("Multiplication: ", results3);
+
+// ! division
+
+const results4 = a / b;
+console.log("Division: ", results4);
+
+//Using Modulus to Find Remainder
+
+const results5 = a % b;
+console.log("Remainder: ", results5);
+
+// ! Increment
+
+let number = 10;
+
+number++; //incrase by 1
+console.log("increment " + number);
+
+// ! Decrement
+let number2 = 10;
+number2--; //decrease by 1
+console.log("decrement " + number2);
+
+//Compound Assignment with Arithmetic Operators
+
+let score = 5;
+score -= 2; // score - 2
+console.log("Compound Assignment with Arithmetic Operators: " + score); // score = 3
+
+score *= 10; // score(3) * 10
+console.log("Compound Assignment with Arithmetic Operators: " + score);
