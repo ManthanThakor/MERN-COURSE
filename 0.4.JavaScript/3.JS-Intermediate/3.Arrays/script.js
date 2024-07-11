1c26bfacf94ae7749cd8cb7c6aaef7d82df41356
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-11 11:08:32 +0530

    MERN COURSE : Java Script Array 2

--- 0.4.JavaScript/3.JS-Intermediate/3.Arrays/script.js
+++ 0.4.JavaScript/3.JS-Intermediate/3.Arrays/script.js
@@ -1,59 +1,72 @@
+console.log(" ");
+
 //-------
-//Creating Array using Array Literal
+//! Creating Array using Array Literal
 //-------
 
 //synatx
 
-const studentAges = [20,30, 25, 50, 31, 20, 10]
-//Access the first elemen
-const firstAge = studentAges[studentAges.length -1]
+const studentAges = [12, 23, 21, 22, 50, 54, 11];
+// console.log(studentAges);
 
+//! Access the first elemen
 
-//Declaring an array of strings
-const daysOfTheWeek = ['Monday','Tuesday','Wednesday','Thursday','friday']
+const studentAgess = [12, 23, 21, 22, 50, 54, 11];
+const firstAge = studentAgess[studentAgess.length - 1];
+// console.log(firstAge);
+
+//! Declaring an array of strings
+const daysOfTheWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 // console.log(daysOfTheWeek);
 
-//Declaring an array of mix types
+//! Declaring an array of mix types
 
-const mixedTypes = [42, 'life',true]
+const mixedTypes = [42, "hi", true];
 // console.log(mixedTypes);
 
-//Declaring an array of with data
-const emptyArr = []
+//! Declaring an array of with data
 
+const emptyArr = [];
+// console.log(emptyArr);
 
-//-----
-//Creating Array using Array Constructor
-//----
+//*-----
+//? Creating Array using Array Constructor
+//*----
 
-//Create an array with empty data
+//! Create an array with empty data
 const emptyArray = new Array();
 
 //Creating an array with a predefined size (5)
-const sizeDefinedArr = new Array(5)
+const sizeDefinedArr = new Array(5);
 
 //Creating an array with elements
-const fruitArr = new Array('Apple','Banana','Kiwi')
+const fruitArr = new Array("Apple", "Banana", "Kiwi");
 
 //Creating an array with mixed types
-const mixDataType = new Array(1, false,'Emmanuel')
-
+const mixDataType = new Array(1, false, "Emmanuel");
 
 //----
 //Creating Array using Array.of()
 //----
 // Creating an array with numbers
 
-const numArr = Array.of(1,2,3,4)
+const numArr = Array.of(1, 2, 3, 4);
 
 //Creating an array with strings
 
-const strArr = Array.of('Apple','Banana','Kiwi')
+const strArr = Array.of("Apple", "Banana", "Kiwi");
 
 // Creating an array with mixed types
-const mixedArr = Array.of('Apple','Banana','Kiwi', false, true, 20)
-
+const mixedArr = Array.of("Apple", "Banana", "Kiwi", false, true, 20);
 
 //---
 //Creating Array using Array.from()
@@ -61,13 +74,17 @@ const mixedArr = Array.of('Apple','Banana','Kiwi', false, true, 20)
 
 //cloning array
 
-const originalArr  =[1,2,3]
-const clonedArr = Array.from(originalArr)
+const originalArr = [1, 2, 3];
+const clonedArr = Array.from(originalArr);
 
 //creating an array from an array-like object
-const arrayLike = {0:'a', 1:'b', 2:'c', length:3}
-const convertedArr = Array.from(arrayLike)
-
+const arrayLike = {
+  0: "a",
+  1: "b",
+  2: "c",
+  length: 3,
+};
+const convertedArr = Array.from(arrayLike);
 
 //-----
 //Using push method
@@ -78,34 +95,32 @@ const convertedArr = Array.from(arrayLike)
 // console.log('modfied fruits', fruits);
 
 // Using .unshift() to add elements at the beginning of an array
-const fruits = ['Apple','Banana']
+const fruits = ["Apple", "Banana"];
 // console.log('Original fruits', fruits);
-const newArrayOfFruits = fruits.unshift('Kiwi','Mango')
+const newArrayOfFruits = fruits.unshift("Kiwi", "Mango");
 // console.log('modfied fruits', fruits);
-
 
 //----
 //Remove Elements (.pop(), .shift())
 //-----
 
-const animal = ['Cat','Dog','Elephant']
+const animal = ["Cat", "Dog", "Elephant"];
 // console.log('Original Animal', animal);
 //pop()
 // const popedAnimal = animal.pop()
-const shiftedAnimal = animal.shift()
+const shiftedAnimal = animal.shift();
 // console.log('After Poped Animal', animal);
 
 //-----
 //Array Size (.length)
 //-----
 
-const colors = ['Red','Yellow','Green','Black']
-const arrayLength = colors.length
+const colors = ["Red", "Yellow", "Green", "Black"];
+const arrayLength = colors.length;
 
 //Modify the length of the array
-colors.length = 2
+colors.length = 2;
 // console.log(colors);
-
 
 //----
 //For Loops
@@ -180,79 +195,77 @@ colors.length = 2
 //   console.log(newNumArrSquare);
 //   console.log(numbers);
 
+// Using map to convert an array of strings to uppercase
+const words = ["apple", "pear", "mango"];
 
-  // Using map to convert an array of strings to uppercase
-  const words = ['apple','pear','mango']
-
-  const convertedWords = words.map(function(word){
-    return word.toUpperCase()
-  })
+const convertedWords = words.map(function (word) {
+  return word.toUpperCase();
+});
 
 //   console.log(convertedWords);
 
 //---
 //Filtering (.filter())
 //---
-const numbers  = [1,2,3,4,5,6,]
+const numbers = [1, 2, 3, 4, 5, 6];
 
 // const evenNumbers = numbers.filter(function(num){
 //     //filter logic here
 //     return num %2 ===0
 // })
 // console.log(evenNumbers);
-const gender = ['male','female']
+const gender = ["male", "female"];
 
-const maleArr = gender.filter(function(gender){
-    return gender ==='male'
-})
+const maleArr = gender.filter(function (gender) {
+  return gender === "male";
+});
 // Using filter to remove falsy values from an array
-const mixedArr2 =  [0,1,'apple',"",null,'banana']
-const truthyValues = mixedArr2.filter(function(value){
-    return Boolean(value)
-})
+const mixedArr2 = [0, 1, "apple", "", null, "banana"];
+const truthyValues = mixedArr2.filter(function (value) {
+  return Boolean(value);
+});
 
 //----
 //Reducing (.reduce())
 //----
 // Using reduce to sum up all elements in an array
 
-const numbers2  = [1,2,3,4,5,6,30]
+const numbers2 = [1, 2, 3, 4, 5, 6, 30];
 
-const sum = numbers2.reduce(function(acc, current){
+const sum = numbers2.reduce(function (acc, current) {
   //perfomr our logic
-  return acc + current
-},0 )
+  return acc + current;
+}, 0);
 
 // Using reduce to find the maximum value in an array
-const maxNum = numbers2.reduce(function(acc, current){
-if(current > acc){
-  return current
-}else{
-  return acc
-}
-},0)
+const maxNum = numbers2.reduce(function (acc, current) {
+  if (current > acc) {
+    return current;
+  } else {
+    return acc;
+  }
+}, 0);
 
 // Using reduce to count the frequency of elements in an array
-const fruits2 = ['Apple','Banana','Apple','Mango','Apple','Mango']
-const fruitCount = fruits2.reduce(function(acc, current){
- if(acc[current]){
-  acc[current] += 1
- }else{
-  acc[current] =1
- }
- return acc
-},{})
-
+const fruits2 = ["Apple", "Banana", "Apple", "Mango", "Apple", "Mango"];
+const fruitCount = fruits2.reduce(function (acc, current) {
+  if (acc[current]) {
+    acc[current] += 1;
+  } else {
+    acc[current] = 1;
+  }
+  return acc;
+}, {});
 
 //----
 //Multi-Dimensional Arrays
 //----
 // Creating a 2D array
 const matrix = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9]
-]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 //Access the elements
 //console.log(matrix[0][0]);
 // console.log(matrix[2][2]);
@@ -260,10 +273,14 @@ const matrix = [
 // Creating a 3D array
 const threeDArray = [
   [
-    [1,2],[3,4]],
-    [[5,6],[7,8]
-  ]
-]
+    [1, 2],
+    [3, 4],
+  ],
+  [
+    [5, 6],
+    [7, 8],
+  ],
+];
 
 console.log(threeDArray[0][1][1]);
 
