239bb402fcf1b4dbfd5dbef446b9f50352c4f1ac
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-11 16:45:37 +0530

    MERN COURSE : Java Script Object 2

--- 0.4.JavaScript/3.JS-Intermediate/4.Objects/script.js
+++ 0.4.JavaScript/3.JS-Intermediate/4.Objects/script.js
@@ -1,25 +1,16 @@
 //---
-//Creating Object using Object Literals
+//?  Creating Object using Object Literals
 //---
 
-// Define an object to represent a book in a bookstore
+//! Define an object to represent a book in a bookstore
 
-// const book = {
-//     title:'Harry potter',
-//     author:'J.K Rowling',
-//     available:true,
-//     price:9.99
-// }
-//---
-//Access object properties using the dot notation
-//---
-
-// const bookTitle = book.title
-// console.log(book.author);
-// console.log(book.price);
-// console.log(book.available);
-// console.log(book.title);
-
+const books = {
+  title: "Harry Potter",
+  author: "J,K Rowling",
+  available: true,
+  price: 9.99,
+};
+// console.log(books);
 
 //title
 //author
@@ -28,27 +19,36 @@
 //pages
 
 //---
-//Accessing Object Properties Using Bracket Notation
+//! Access object properties using the dot notation
+//---
+
+const booktitle = books.title;
+// console.log(booktitle);
+// console.log(books.author);
+// console.log(books.price);
+// console.log(books.available);
+
+//---
+//! Accessing Object Properties Using Bracket Notation
 //--
 
 const book = {
-    title:'Harry potter',
-    author:'J.K Rowling',
-    available:true,
-    price:9.99
-}
+  title: "Harry potter",
+  author: "J.K Rowling",
+  available: true,
+  price: 9.99,
+};
 
-const bookTitle = book["title"]
+const bookTitle = book["title"];
 // console.log(book['available']);
 // console.log(book['price']);
 // console.log(book['author']);
 
-
 //Properties with Special Characters
 const person = {
-    'first name':'Ben',
-    'last name':'Joe'
-}
+  "first name": "Ben",
+  "last name": "Joe",
+};
 
 // console.log(person['first name']);
 // console.log(person['last name']);
@@ -56,11 +56,11 @@ const person = {
 // Using Expressions
 
 const task = {
-    description:'Finish homework',
-    isCompleted:false
-}
+  description: "Finish homework",
+  isCompleted: false,
+};
 
-const query = 'isCom' + 'pleted'
+const query = "isCom" + "pleted";
 // console.log(task[query]);
 
 //---
@@ -70,36 +70,36 @@ const query = 'isCom' + 'pleted'
 //Add user role
 
 const userProfile = {
-    username:'Masynctech',
-    email:'masunctech@gmail.com',
-    // portfolio: {
-    //     work1:'Landing page',
-    //     work2:'Web App'
-    // }
-}
+  username: "Masynctech",
+  email: "masunctech@gmail.com",
+  // portfolio: {
+  //     work1:'Landing page',
+  //     work2:'Web App'
+  // }
+};
 //add role property
 
 // userProfile.role = 'admin';
-userProfile['role'] = 'superAdmin'
+userProfile["role"] = "superAdmin";
 
 // Adding Properties with Special Characters
 const book1 = {
-    title:'Harry potter',
-}
+  title: "Harry potter",
+};
 
-book1['publication year'] = 2025;
+book1["publication year"] = 2025;
 
 //Adding Nested Properties
 
 const employee = {
-    name:'Bob',
-    department:'HR'
-}
+  name: "Bob",
+  department: "HR",
+};
 //add address
 employee.address = {
-    city:'Kumasi',
-    state:'Ghana'
-}
+  city: "Kumasi",
+  state: "Ghana",
+};
 // console.log(employee.address.state);
 
 //--
@@ -140,16 +140,15 @@ employee.address = {
 //--
 
 const user = {
-    username:'masynctech',
-    password:'12345',
-    gender:'male',
-    location:'New York',
-    plan:'starter',
-    postCount:1
-}
+  username: "masynctech",
+  password: "12345",
+  gender: "male",
+  location: "New York",
+  plan: "starter",
+  postCount: 1,
+};
 
 //update the user plan
-
 
 // if(user.postCount >= 10){
 //     user.plan ='Premium'
@@ -157,11 +156,10 @@ const user = {
 //     user.plan = 'Basic'
 // }
 
-
-if(user.postCount >= 10){
-    user['plan'] ='Premium'
-}else{
-    user['plan'] = 'Basic'
+if (user.postCount >= 10) {
+  user["plan"] = "Premium";
+} else {
+  user["plan"] = "Basic";
 }
 
 // console.log(user);
@@ -170,41 +168,41 @@ if(user.postCount >= 10){
 //Looping Through Objects using for...in Loop
 //--
 
-const student ={
-   name:'Thomas',
-   age:20,
-   grade:'A'
-}
+const student = {
+  name: "Thomas",
+  age: 20,
+  grade: "A",
+};
 
-for(const objeckKey in student){
-    // console.log(objeckKey);
-   if(student.hasOwnProperty(objeckKey)){
-   // console.log(`${objeckKey}: ${student[objeckKey]}`);
-   }
+for (const objeckKey in student) {
+  // console.log(objeckKey);
+  if (student.hasOwnProperty(objeckKey)) {
+    // console.log(`${objeckKey}: ${student[objeckKey]}`);
+  }
 }
 
 //Loop to Create a New Object
 const numbers = {
-    one:1,
-    two:2,
-    three:3
-}
-const doubledNumbers = {}
+  one: 1,
+  two: 2,
+  three: 3,
+};
+const doubledNumbers = {};
 
-for(const key in numbers){
-    if(numbers.hasOwnProperty(key)){
-        doubledNumbers[key] = numbers[key] * 2
-    }
+for (const key in numbers) {
+  if (numbers.hasOwnProperty(key)) {
+    doubledNumbers[key] = numbers[key] * 2;
+  }
 }
 
 //Loop Through Nested Objects
 const school = {
-    name:'Maynsynctech School',
-    students:{
-        course1:'Javascript',
-        course2:'Node JS'
-    }
-}
+  name: "Maynsynctech School",
+  students: {
+    course1: "Javascript",
+    course2: "Node JS",
+  },
+};
 
 // for(const key in school){
 //     if(typeof school[key] ==='object'){
@@ -218,17 +216,17 @@ const school = {
 
 // Loop to Filter Object Properties
 const scores = {
-    math:90,
-    science:85,
-    literature:92
-}
+  math: 90,
+  science: 85,
+  literature: 92,
+};
 
-const passedSub ={}
+const passedSub = {};
 
-for(const subject in  scores){
-    if(scores[subject] >= 90){
-        passedSub[subject] = scores[subject]
-    }
+for (const subject in scores) {
+  if (scores[subject] >= 90) {
+    passedSub[subject] = scores[subject];
+  }
 }
 
 // console.log(passedSub);
@@ -237,26 +235,25 @@ for(const subject in  scores){
 //Looping Through Objects using Object.keys()
 //--
 const person1 = {
-    name:'Alice',
-    age: 30,
-    occupation:'Engineer'
-}
+  name: "Alice",
+  age: 30,
+  occupation: "Engineer",
+};
 
-const keysArr = Object.keys(person1)
+const keysArr = Object.keys(person1);
 
 //using foreach to loop
-keysArr.forEach(function(key){
-    //console.log(`${key}: ${person1[key]}`);
-})
+keysArr.forEach(function (key) {
+  //console.log(`${key}: ${person1[key]}`);
+});
 
 //Filtering Object Properties
 const scores1 = {
-    math:90,
-    science:85,
-    literature:90,
-    programming:100
-
-}
+  math: 90,
+  science: 85,
+  literature: 90,
+  programming: 100,
+};
 
 // const highScores = Object.keys(scores1)
 
@@ -271,27 +268,30 @@ const scores1 = {
 
 //Use chaining
 
-const highestScore = Object.keys(scores1).filter(function(key){
-    return scores1[key] >= 85
-}).reduce(function(obj, key){
-    return obj[key] = scores1[key]
-},{})
-
+const highestScore = Object.keys(scores1)
+  .filter(function (key) {
+    return scores1[key] >= 85;
+  })
+  .reduce(function (obj, key) {
+    return (obj[key] = scores1[key]);
+  }, {});
 
 //Modifying Object Properties
 
 const prices = {
-    apple:1,
-    banana:0.5,
-    orange:0.8
-}
-const dicountedPrices = Object.keys(prices).map(function(key){
+  apple: 1,
+  banana: 0.5,
+  orange: 0.8,
+};
+const dicountedPrices = Object.keys(prices)
+  .map(function (key) {
     return {
-        [key]: prices[key] * 0.9
-    }
-}).reduce(function(acc, curr){
-    return Object.assign(acc, curr)
-},{})
+      [key]: prices[key] * 0.9,
+    };
+  })
+  .reduce(function (acc, curr) {
+    return Object.assign(acc, curr);
+  }, {});
 
 // console.log(dicountedPrices);
 
@@ -300,29 +300,29 @@ const dicountedPrices = Object.keys(prices).map(function(key){
 //--
 
 const person2 = {
-    name:'Alice',
-    age: 30,
-    occupation:'Engineer'
-}
+  name: "Alice",
+  age: 30,
+  occupation: "Engineer",
+};
 
-const values = Object.values(person2)
+const values = Object.values(person2);
 
-values.forEach(function(val){
-    //console.log(val);
-})
+values.forEach(function (val) {
+  //console.log(val);
+});
 
 //Calculating Sum of Values
 const numbers1 = {
-    a:10,
-    b:2,
-    c:3
-}
+  a: 10,
+  b: 2,
+  c: 3,
+};
 
-const numValues = Object.values(numbers1)
+const numValues = Object.values(numbers1);
 
-const total= numValues.reduce(function(acc, val){
-        return acc + val
-},0)
+const total = numValues.reduce(function (acc, val) {
+  return acc + val;
+}, 0);
 
 // console.log(total);
 
@@ -331,14 +331,14 @@ const total= numValues.reduce(function(acc, val){
 //--
 
 const person3 = {
-    name:'Alice',
-    age: 30,
-    occupation:'Engineer'
-}
-const entries = Object.entries(person3)
+  name: "Alice",
+  age: 30,
+  occupation: "Engineer",
+};
+const entries = Object.entries(person3);
 
-entries.forEach(function([key, value]){
-       console.log(key);
-       console.log(value);
-       console.log(`${key}:${value}`);
-})
+entries.forEach(function ([key, value]) {
+  //   console.log(key);
+  //   console.log(value);
+  //   console.log(`${key}:${value}`);
+});
