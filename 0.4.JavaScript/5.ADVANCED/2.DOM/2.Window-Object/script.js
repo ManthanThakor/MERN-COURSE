53a4fc4baba8e1dc341c4094e9b860384c7e67ea
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-13 11:06:57 +0530

    MERN COURSE : Java Scrit Advance 12

--- 0.4.JavaScript/5.ADVANCED/2.DOM/2.Window-Object/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/2.Window-Object/script.js
@@ -1,3 +1,20 @@
 //! ======================
 //? Window object
 //! ======================
+
+console.log(window);
+console.log(window.document);
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+console.log(window.location);
+console.log(window.location.host);
+console.log(window.location.hostname);
+console.log(window.history);
+console.log(window.navigator);
+console.log(window.screen);
+// console.log(window.alert("RUNNNNNNNN"));
+// console.log(window.confirm("hi"));
+// window.prompt();
+// window.open("https://google.com");
+console.log(innerHeight);
+console.log(document.body);
