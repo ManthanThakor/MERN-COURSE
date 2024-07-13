4a76d145ead3310e91f7171eaf53380822f532e2
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-13 11:17:23 +0530

    MERN COURSE : Java Scrit Advance 14

--- 0.4.JavaScript/5.ADVANCED/2.DOM/3.getElementById/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/3.getElementById/script.js
@@ -1,3 +1,15 @@
 //! ====================
 //? Selecting elements using getElementById
 //! ====================
+
+const conferencetitle = document.getElementById("conferenceTitle");
+// console.log(conferencetitle.innerHTML);
+
+const confeDate3 = document.getElementById("conferenceDate");
+console.log(confeDate3);
+const confeDate1 = document.getElementById("locationTitle");
+console.log(confeDate1);
+const confeDate2 = document.getElementById("speakersTitle");
+console.log(confeDate2);
+const confeDate = document.getElementById("speakerList");
+console.log(confeDate);
