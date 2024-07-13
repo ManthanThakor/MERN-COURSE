941c213ef8405ff08d2492ecccc168fc2860f84e
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-13 14:26:04 +0530

    MERN COURSE : Java Scrit Advance 19

--- 0.4.JavaScript/5.ADVANCED/2.DOM/6.querySelectorAll/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/6.querySelectorAll/script.js
@@ -1,28 +1,31 @@
-// Select all product cards
+// ! Select all product cards
 const productCardEls = document.querySelectorAll(".product-card");
+// console.log(productCardEls);
 
-// Extract and log names of all products
+//!  Extract and log names of all products
+
 const productNameEls = document.querySelectorAll(".product-name");
+// console.log(productNameEls);
 
 productNameEls.forEach((el) => {
   // console.log(el.innerHTML);
 });
 
-// Select and log all prices of laptop category products
+//!  Select and log all prices of laptop category products
+
 const laptopPricesEl = document.querySelectorAll(
   '[data-category="laptop"] .product-price'
 );
-
 // console.log(laptopPricesEl);
 
 laptopPricesEl.forEach((el) => {
-  //console.log(el.textContent);
+  // console.log(el.textContent);
 });
 
-// Select all products with a rating above 4.6
+//!  Select all products with a rating above 4.6
 
 const highestRatingsEl = document.querySelectorAll(
   '[data-rating="4.7"], [data-rating="4.8"]'
 );
 
-highestRatingsEl.forEach((el) => console.log(el.innerHTML));
+// highestRatingsEl.forEach((el) => console.log(el.innerHTML));
