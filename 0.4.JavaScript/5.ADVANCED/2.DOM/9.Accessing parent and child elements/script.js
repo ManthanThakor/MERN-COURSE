7d826afc51600e930dd694c4c37487365cf3acfb
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-14 20:56:31 +0530

    MERN COURSE : Java Scrit Advance 26

--- 0.4.JavaScript/5.ADVANCED/2.DOM/9.Accessing parent and child elements/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/9.Accessing parent and child elements/script.js
@@ -1,19 +1,24 @@
 //! Accessing Parent Elements
 
 const parentEl = document.getElementById("parent");
+// console.log(parentEl);
 
-//Accessing the grandparent element
-const grandparentEl = parentEl.parentNode;
+//! Accessing the grandparent element
+
+// const grandparentEl = parentEl.parentNode;
+// console.log(grandparentEl);
 
 //Accessing Child Elements
+
 const firstChildEl = parentEl.firstChild;
-const lastChildEl = parentEl.lastChild;
-
-// console.log(lastChildEl);
 // console.log(firstChildEl);
+const lastChildEl = parentEl.lastChild;
+// console.log(lastChildEl);
 
-// Accessing Element Children Only
+//! Accessing Element Children Only
+
 const firstElmentChild = parentEl.firstElementChild;
+// console.log(firstElmentChild);
+
 const lastElementChild = parentEl.lastElementChild;
 // console.log(lastElementChild);
-// console.log(firstElmentChild);
