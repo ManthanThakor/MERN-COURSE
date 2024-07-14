a95bfbec54c393870677fbad5a667f17f5ec3b50
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-14 14:18:46 +0530

    MERN COURSE : Java Scrit Advance 25

--- 0.4.JavaScript/5.ADVANCED/2.DOM/8.getElementByName/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/8.getElementByName/script.js
@@ -1,2 +1,17 @@
+function collectData() {
+  const languageP = document.getElementsByName("languageProficiency");
+  console.log(languageP);
 
+  // loop through the nodeList to find the selected radio button
 
+  for (let i = 0; i < languageP.length; i++) {
+    if (languageP[i].checked) {
+      console.log(`Selected language is: ${languageP[i].value}`);
+    }
+  }
+
+  // log all elements  with name "email"
+
+  const emailEl = document.getElementsByName("email");
+  console.log(emailEl);
+}
