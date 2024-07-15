3b5b232ca779d8ba58b2f754d2c31cc8618f39bb
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 10:56:45 +0530

    MERN COURSE : Java Scrit Advance 34

--- 0.4.JavaScript/5.ADVANCED/2.DOM/13.previousSibling/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/13.previousSibling/script.js
@@ -1,5 +1,25 @@
 //! select buttons
 
+const button1el = document.getElementById("button1");
+console.log(button1el);
+
+const button2el = document.getElementById("button2");
+console.log(button2el);
+
 //! use previousSibling to get the preceeding paragraph element
 
+const text1El = button1el.previousSibling;
+// console.log(text1El);
+
+const text2El = button2el.previousSibling;
+// console.log(text2El);
+
 //! change the text content of the preceeding paragraph
+
+if (text1El.nodeType === 3) {
+  text1El.textContent = "Text 1 has been updated";
+}
+
+if (text2El.nodeType === 3) {
+  text2El.textContent = "Text 2 has been changed";
+}
