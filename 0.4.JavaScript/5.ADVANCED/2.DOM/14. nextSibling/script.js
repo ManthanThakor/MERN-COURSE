6355fff5f127f524e9d046a6fcb708fdaad8e1e0
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 11:04:34 +0530

    MERN COURSE : Java Scrit Advance 35

--- 0.4.JavaScript/5.ADVANCED/2.DOM/14. nextSibling/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/14. nextSibling/script.js
@@ -1,20 +1,20 @@
-//Get the container element
+//! Get the container element
 
 const containerEl = document.getElementById("container");
+// console.log(containerEl);
 
-// Get all buttons within the container
+//! Get all buttons within the container
 
 const buttonsEl = document.querySelectorAll("button");
-
 // console.log(buttonsEl);
 
-// Loop through each button and change the text of its next sibling
+//! Loop through each button and change the text of its next sibling
 
 buttonsEl.forEach((button) => {
-  const nextElementEl = button.nextSibling;
-  // Check if the next sibling is an element node
-  console.log(nextElementEl);
-  if (nextElementEl && nextElementEl.nodeType === 3) {
-    nextElementEl.textContent = "Changed Text";
+  const nextel = button.nextSibling;
+  //? check if next sibling is element node
+  // console.log(nextel);
+  if (nextel && nextel.nodeType === 3) {
+    nextel.textContent = "Changed Text";
   }
 });
