ed14e8905ef27069cb4593e9a7eaa17048ef97ab
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 13:37:38 +0530

    MERN COURSE : Java Scrit Advance 44

--- 0.4.JavaScript/5.ADVANCED/2.DOM/20.createElement/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/20.createElement/script.js
@@ -1,22 +1,10 @@
-// Function to create a new task
+//! Function to create a new task
 
 function createTask(newTask) {
-  // Create a new div element to hold the task
-  const taskEl = document.createElement("div");
-  //add new css to the created element
+  //! Create a new div element to hold the task
+  //!add new css to the created element
   //   taskEl.setAttribute("class", "task");
-  taskEl.className = "task";
-  //Create a text node containing the task text
-  const taskTextNodeEl = document.createTextNode(newTask);
-  //Append the text node to the div element
-  taskEl.appendChild(taskTextNodeEl);
-  //Finally, append the task to the container div
-  document.getElementById("container").appendChild(taskEl);
-  console.log(taskTextNodeEl);
-  console.log(taskEl);
+  //!Create a text node containing the task text
+  //!Append the text node to the div element
+  //!Finally, append the task to the container div
 }
-
-createTask("Programming");
-createTask("Go Shopping");
-createTask("Meeting");
-createTask("Studying");
