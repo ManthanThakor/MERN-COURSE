80bed121f8448a3617491b30bf9c977fc9dd858e
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 10:37:23 +0530

    MERN COURSE : Java Scrit Advance 31

--- 0.4.JavaScript/5.ADVANCED/2.DOM/12.Using childNodes/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/12.Using childNodes/script.js
@@ -1,16 +1,20 @@
-//Step 1: Select the outer div using its ID
-const outerDivEl = document.getElementById("outerDiv");
+//! Step 1: Select the outer div using its ID
+
+const outerDivEl =
 // console.log(outerDivEl);
 
-//Step 2: Navigate to the first child, which is a text node
-const firstChildEl = outerDivEl.childNodes[0];
+//! Step 2: Navigate to the first child, which is a text node
 
-// Step 3: Navigate to the second child, which is the innerDiv
-const innerDivEl = outerDivEl.childNodes[1];
+// const firstChildEl = 
 
-//Step 4: select the first child of the inner div
-const innerDivFirstChildEl = innerDivEl.childNodes[0];
+// ! Step 3: Navigate to the second child, which is the innerDiv
 
-//Step 4: select the second child of the inner div
-const nestetListEl = innerDivEl.childNodes[1];
-console.log(nestetListEl);
+// const innerDivEl = 
+
+//! Step 4: select the first child of the inner div
+
+// const innerDivFirstChildEl = 
+
+//! Step 4: select the second child of the inner div
+
+// const nestetListEl = 
