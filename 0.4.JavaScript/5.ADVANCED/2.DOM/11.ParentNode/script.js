8375ed91941dc8609ec530547bf22073d6192045
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 10:34:07 +0530

    MERN COURSE : Java Scrit Advance 30

--- 0.4.JavaScript/5.ADVANCED/2.DOM/11.ParentNode/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/11.ParentNode/script.js
@@ -1,7 +1,19 @@
 //Step 1: Select a list item using data attribute
 
+const selectedItem = document.querySelector('[data-item="fruit"]');
+// console.log(selectedItem);
+
 // Step 2: Navigate to the parent <ul> element
+
+const parentListEl = selectedItem.parentNode;
+// console.log(parentListEl);
 
 // Step 3: Navigate to grandparent
 
+const grandparentEl = parentListEl.parentNode;
+// console.log(grandparentEl);
+
 // Step 4: Navigate to great- grandparent
+
+const greatGrandparentEl = grandparentEl.parentNode;
+// console.log(greatGrandparentEl);
