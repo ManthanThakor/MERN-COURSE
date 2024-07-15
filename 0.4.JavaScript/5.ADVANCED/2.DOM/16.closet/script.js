aac077308b50602482f49c883d4264d4ea1d07d5
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 11:09:04 +0530

    MERN COURSE : Java Scrit Advance 37

--- 0.4.JavaScript/5.ADVANCED/2.DOM/16.closet/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/16.closet/script.js
@@ -1,10 +1,5 @@
-// Select the sub-menu item with the id "team"
-const selectedSubMenuItemEl = document.getElementById("team");
+//! Select the sub-menu item with the id "team"
 
-// Find the closest menu item to this sub-menu item
-const parentMenuItemEl = selectedSubMenuItemEl.closest(".menuItem");
+//! Find the closest menu item to this sub-menu item
 
-//Find the closest sub-menu to this sub-menu item
-
-const closetSubMenuEl = selectedSubMenuItemEl.closest(".subMenu");
-console.log(closetSubMenuEl);
+//! Find the closest sub-menu to this sub-menu item
