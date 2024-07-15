a1b192f30c5102d29c6b1a20a8cefe6502ef038d
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 20:48:15 +0530

    MERN COURSE : Java Scrit Advance 57

--- 0.4.JavaScript/5.ADVANCED/2.DOM/28.removeEventListener/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/28.removeEventListener/script.js
@@ -6,8 +6,21 @@ function showAlert() {
 
 //!getting the button element
 
+const alertbtn = document.getElementById("alertButton");
+// console.log(alertbtn);
+
 //!Add addEventListener  to the element
+
+alertbtn.addEventListener("click", showAlert);
 
 //!Named function for  removing event
 
+function removeAlert() {
+  alertbtn.removeEventListener("click", showAlert);
+}
+
 //!getting the remove button element
+
+const removebtn = document.getElementById("removeButton");
+// console.log(removebtn);
+removebtn.addEventListener("click", removeAlert);
