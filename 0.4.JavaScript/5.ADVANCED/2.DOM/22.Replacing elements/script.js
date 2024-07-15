45561a26339aaee85d4344cd59a767d3481dd5b6
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 14:27:46 +0530

    MERN COURSE : Java Scrit Advance 48

--- 0.4.JavaScript/5.ADVANCED/2.DOM/22.Replacing elements/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/22.Replacing elements/script.js
@@ -1,20 +1,30 @@
 // Function to replace an article
 
 function replaceArticle(articleId, newContent) {
-  // Find the existing article element by its ID
+  //!  Find the existing article element by its ID
 
   const existingArticle = document.getElementById(articleId);
-  //Create a new div element for the updated article
+
+  //! Create a new div element for the updated article
+
   const newArticleEl = document.createElement("div");
-  //add css to it
+
+  //! add css to it
   newArticleEl.className = "article";
-  // Keep the ID same for the new article
+  //!  Keep the ID same for the new article
+
   newArticleEl.id = articleId;
-  //Create a text node for the new content
+
+  //! Create a text node for the new content
+
   const newTextNode = document.createTextNode(newContent);
-  //Append the text node to the new article div
+
+  //! Append the text node to the new article div
+
   newArticleEl.appendChild(newTextNode);
-  // Replace the existing article with the new one
+
+  //!  Replace the existing article with the new one
+
   existingArticle.parentNode.replaceChild(newArticleEl, existingArticle);
 }
 
