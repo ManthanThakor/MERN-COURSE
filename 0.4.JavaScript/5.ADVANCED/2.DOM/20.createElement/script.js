fe5ffc1fbe74bfd72311bc1c5b7ecfb3d124cff8
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 13:49:39 +0530

    MERN COURSE : Java Scrit Advance 45

--- 0.4.JavaScript/5.ADVANCED/2.DOM/20.createElement/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/20.createElement/script.js
@@ -2,9 +2,33 @@
 
 function createTask(newTask) {
   //! Create a new div element to hold the task
+
+  const taskEl = document.createElement("div");
+  // console.log(taskEl);
+
   //!add new css to the created element
+
   //   taskEl.setAttribute("class", "task");
+  taskEl.className = "task";
+  // console.log(taskEl);
+
   //!Create a text node containing the task text
+
+  const taskTextNodeEl = document.createTextNode(newTask);
+  // console.log(taskTextNodeEl);
+  // console.log(taskEl);
+
   //!Append the text node to the div element
+
+  taskEl.appendChild(taskTextNodeEl);
+
   //!Finally, append the task to the container div
+
+  document.getElementById("container").appendChild(taskEl);
+  // console.log(taskTextNodeEl);
+  console.log(taskEl);
 }
+createTask("Programming");
+createTask("Driving");
+createTask("Reading");
+createTask("Typing");
