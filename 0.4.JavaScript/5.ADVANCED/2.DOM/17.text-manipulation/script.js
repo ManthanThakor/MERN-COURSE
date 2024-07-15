6c6c30d9a31709687e26afbad2d05b24e2f4977a
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-15 11:38:19 +0530

    MERN COURSE : Java Scrit Advance 39

--- 0.4.JavaScript/5.ADVANCED/2.DOM/17.text-manipulation/script.js
+++ 0.4.JavaScript/5.ADVANCED/2.DOM/17.text-manipulation/script.js
@@ -1,7 +1,26 @@
-//Using textContent to update the blog title
+//! Using textContent to update the blog title
+const blogt = document.getElementById("title");
+// console.log(blogt);
+blogt.textContent = "New Blog Title";
 
-//change the background
+//! change the background
 
-//Using innerHTML to add another paragraph with some HTML content
+blogt.style.background = "blue";
+blogt.style.color = "white";
+blogt.style.textAlign = "center";
 
-// Using innerText to modify the comment section (assume the original comment is visible)
+//! Using innerHTML to add another paragraph with some HTML content
+
+const paragraphel = document.getElementById("paragraph");
+console.log(paragraphel);
+paragraphel.innerHTML = "<h1>I have changed the paragraph</h1>";
+paragraphel.innerHTML +=
+  "<br><strong> This is new Paragraph text </strong></br>";
+
+//! Using innerText to modify the comment section (assume the original comment is visible)
+
+const firstCommentEl = document
+  .getElementById("comment1")
+  .querySelector(".commentText");
+
+firstCommentEl.innerText = "This is a new comment";
