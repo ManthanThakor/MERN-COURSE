3d9ec1339700dda93f9913d633f47e383d9ffb4a
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-16 14:37:44 +0530

    MERN COURSE : Java Scrit Advance 64

--- 0.4.JavaScript/5.ADVANCED/3.Asynchronous Programming/3.callback/script.js
+++ 0.4.JavaScript/5.ADVANCED/3.Asynchronous Programming/3.callback/script.js
@@ -2,21 +2,71 @@
 
 //! create callback function
 
-function mycallbackfn() {
-  console.log("Hello this is a simple callback function");
+function myCallbackfn() {
+  // console.log("Hello this is a simple callback function");
 }
 
 //! Usage
 
+function logMessage(callback) {
+  //execute the callback fn
+  callback();
+}
+
+//! call HOF
+
+// logMessage(myCallbackfn);
+
+logMessage(function () {
+  // console.log("Hello this is a simple callback function");
+});
+
+//!: Returning a callback function method 1
+
+function logMessageHOF(callback) {
+  //execute the callback fn
+  const result = callback();
+  // console.log(callback());
+}
+
+logMessageHOF(() => {
+  // return "Hello this is a  simple callback function";
+});
+
+//! Extracting Callback function
+
+function logMessageHOF(callback) {
+  //execute the callback fn
+  const result = callback();
+  return result;
+}
+
+const result = logMessageHOF(() => {
+  return "Hello this is a  simple callback function";
+});
+// console.log(result);
+
+//! passing parameters to the callback function
+
+const log = (callback) => {
+  const result = callback("ANG", "wkd");
+  // console.log(result);
+};
+const logsCallbackfn = (firstName, lastName) => {
+  return `hello ${firstName} ${lastName} this is a simple cb`;
+};
+
 //call HOF
 
-//: Returning a callback function method 1
+log(logsCallbackfn);
 
-//Extracting Callback function
+//! HOF with parameter and call back function
 
-//passing parameters to the callback function
+const calculateSum = (a, b, callback) => {
+  const result = a + b;
+  callback(result);
+};
 
-//call HOF
-//logMessageHOF(logMessageCallbackFn);
-
-//HOF with parameter and call back function
+calculateSum(3, 5, (result) => {
+  console.log(`The sum is: ${result}`);
+});
