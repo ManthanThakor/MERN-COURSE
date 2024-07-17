a26098bda28a9ebe8bebc851076bf95813ed1f55
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-17 13:21:47 +0530

    MERN COURSE : Java Scrit Advance 73

--- 0.4.JavaScript/5.ADVANCED/3.Asynchronous Programming/8.Async-Await/script.js
+++ 0.4.JavaScript/5.ADVANCED/3.Asynchronous Programming/8.Async-Await/script.js
@@ -26,3 +26,37 @@ getProcessValue();
 //! =================
 //? Fetching Multiple Values in Parallel
 //! =================
+
+const getName = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("John Doe");
+    }, 2000);
+  });
+};
+
+const getAge = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(30);
+    }, 1000);
+  });
+};
+
+//!  method-1
+
+/* const fetchData = async () => {
+   const name = await getName();
+  const age = await getAge();
+   console.log(`Name: ${name}, Age: ${age}`);
+ };
+ fetchData(); */
+
+//!  method-2
+async function getuserDetails() {
+  const result = await Promise.all([getName(), getAge()]);
+  console.log(result);
+}
+getuserDetails();
+
+//! =================
