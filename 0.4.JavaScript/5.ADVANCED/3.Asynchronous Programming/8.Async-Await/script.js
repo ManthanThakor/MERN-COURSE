f6f84306e6ab703a3d4516ae70b395bbead05765
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-17 13:14:13 +0530

    MERN COURSE : Java Scrit Advance 72

--- 0.4.JavaScript/5.ADVANCED/3.Asynchronous Programming/8.Async-Await/script.js
+++ 0.4.JavaScript/5.ADVANCED/3.Asynchronous Programming/8.Async-Await/script.js
@@ -1,7 +1,28 @@
+//! =================
 //? Simulating a Delay
+//! =================
 
 //! creating
+async function waitAndRun(value) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(value);
+    }, 3000);
+  });
+}
+
+// const waitAndRun = async () => {
+//   console.log("hi");
+// };
 
 //! usage
 
+const getProcessValue = async () => {
+  const result = await waitAndRun("This is my promise with async await");
+  console.log(result);
+};
+getProcessValue();
+
+//! =================
 //? Fetching Multiple Values in Parallel
+//! =================
