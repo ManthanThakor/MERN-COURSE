ca255ab55cefbf8c9983b9715d54a58b844340fb
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-18 14:30:14 +0530

    MERN COURSE : Node Js4

--- 5.NODEJS/1.GETTING-STARTED/1.my-first-app/app.js
+++ 5.NODEJS/1.GETTING-STARTED/1.my-first-app/app.js
@@ -1,1 +1,13 @@
+//! ========================
+
 console.log("Hello world");
+console.log("how are you ");
+console.log("I'm fine thank you!");
+console.log("This is a test");
+console.log("This is a test with multiple arguments");
+console.log("Hello", "World", "!");
+console.log("This is a test with an object");
+console.log({ name: "John Doe", age: 30 });
+console.log("This is a test with a function");
+
+//! ========================
