d695e216a8c5ea4abeec95ebe4001e776f6714e4
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-18 20:06:28 +0530

    MERN COURSE : Node Js7\8

--- 5.NODEJS/1.GETTING-STARTED/3.node-process/app.js
+++ 5.NODEJS/1.GETTING-STARTED/3.node-process/app.js
@@ -2,14 +2,24 @@
 //? analyzing process
 //! =====================
 
-const os = require("os");
-const { sayHi } = require("./me");
-//Environment variables
+// console.log(process);
 
-//Accessing env varibales
+// const os = require("os");
+// const { sayHi } = require("./me");
+
+//! =====================
+//? Environment variables
+//! =====================
+
+// console.log(process.env);
+
+//! Accessing env varibales
+
 const appEnv = process.env.APP_ENV || "development";
-//Display the env
-//console.log(`Our Node App is running ${appEnv}`);
+
+//! Display the env
+console.log(`Our Node App is running ${appEnv}`);
+
 //setting the env
 
 //! process.exit()
@@ -26,24 +36,24 @@ const appEnv = process.env.APP_ENV || "development";
 
 //Log the current working directory of the process
 
-console.log(`Current working directory ${process.cwd()}`);
+// console.log(`Current working directory ${process.cwd()}`);
 
-try {
-  //change the current working directory
-  process.chdir("/tmp");
-  // Log the new current working directory after the change
-  console.log(`The new working directory is ${process.cwd()}`);
-} catch (error) {
-  //Log any error that might occur during the directory change
-  console.log(`Something happpened: ${error}`);
-}
+// try {
+//   //change the current working directory
+//   process.chdir("/tmp");
+//   // Log the new current working directory after the change
+//   console.log(`The new working directory is ${process.cwd()}`);
+// } catch (error) {
+//   //Log any error that might occur during the directory change
+//   console.log(`Something happpened: ${error}`);
+// }
 
-const sandwich = "Ham and Cheese";
-const drink = "Lemonade";
+// const sandwich = "Ham and Cheese";
+// const drink = "Lemonade";
 
-// Named exports using the exports shortcut
-exports.sandwich = sandwich;
+// // Named exports using the exports shortcut
+// exports.sandwich = sandwich;
 
-// Default export using module.exports
+// // Default export using module.exports
 
-console.log(sayHi(3));
+// console.log(sayHi(3));
