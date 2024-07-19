362a8557e0d8b9b9b840b38785bc966f9ba4a63c
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 20:28:39 +0530

    MERN COURSE : Node Js 33

--- 5.NODEJS/8.Employees-API-Using-NodeJS/server.js
+++ 5.NODEJS/8.Employees-API-Using-NodeJS/server.js
@@ -1,48 +1,51 @@
 //! =====================================
 //? =====  IMPORTS Required modules =====
 //! =====================================
+
 const http = require("http");
-//mimic (database)
+
+//! mimic (database)
+
 const employees = [
   { id: 1, name: "Emmanuel" },
   { id: 2, name: "Agnes" },
 ];
+
 //! =====================================
 //? =====  Define The Handler =====
 //! =====================================
+
 const requestHandler = (req, res) => {
   const { method, url } = req;
   const parts = url.split("/");
   const id = parts[2];
-  //!Get all employees
+  //! Get all employees
   if (method === "GET" && url === "/employees") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(employees));
   }
-  //!Get single employee
+  //! Get single employee
   else if (method === "GET" && parts[1] === "employees" && id) {
     const employee = employees.find((emp) => emp.id === parseInt(id));
     if (employee) {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(employee));
     } else {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ message: "Employee not found" }));
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Employee not found" }));
     }
   }
 
   //! Create new employee
-  else if (method === "POST" && url === "/employees") {
-    let body = "";
-    //Listen to the event of making post reques
+  else if (method === "POST" && URL === "/employees") {
+    const body = "";
     req.on("data", (chunk) => {
       body += chunk;
     });
-    //Send the response
     req.on("end", () => {
       const newEmployee = JSON.parse(body);
       employees.push(newEmployee);
-      res.writeHead(200, { "Content-Type": "application/json" });
+      res.writeHead(201, { "Content-Type": "application/json" });
       res.end(
         JSON.stringify({
           newEmployee,
@@ -56,11 +59,13 @@ const requestHandler = (req, res) => {
 //! =====================================
 //? =====  Create The Server =====
 //! =====================================
+
 const server = http.createServer(requestHandler);
 
 //! =====================================
 //? =====  Start The Server =====
 //! =====================================
+
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`The server is running on http://localhost:${PORT}`);
