5a21a1f91e70ab381252adc02c05a232dbbb8722
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 10:14:29 +0530

    MERN COURSE : Node Js 10

--- 5.NODEJS/1.GETTING-STARTED/3.node-process/app.js
+++ 5.NODEJS/1.GETTING-STARTED/3.node-process/app.js
@@ -4,9 +4,6 @@
 
 // console.log(process);
 
-// const os = require("os");
-// const { sayHi } = require("./me");
-
 //! =====================
 //? Environment variables
 //! =====================
@@ -19,42 +16,35 @@ const appEnv = process.env.APP_ENV || "development";
 
 //! Display the env
 
-console.log(`Our Node App is running ${appEnv}`);
+//? console.log(`Our Node App is running ${appEnv}`);
 
-//! setting the env
+//! =====================
+//? setting the env
+//! =====================
 
 //! process.exit()
-// Check the current environment using the 'NODE_ENV' environment variable
 
-// if (process.env.NODE_ENV !== "production") {
-//   //Display some error
-//   //console.log("This script should only run in production");
-//   //Exit the process
-//   process.exit();
-// }
+//! Check the current environment using the 'NODE_ENV' environment variable
+
+if (process.env.NODE_ENV !== "production") {
+  // Display some error
+  //?  console.log("This script should only run in production");
+  // Exit the process
+  //? process.exit();
+}
 
 //?`process.cwd()` and `process.chdir(directory)
 
 //Log the current working directory of the process
 
-// console.log(`Current working directory ${process.cwd()}`);
+console.log(`Current working directory ${process.cwd()}`);
 
-// try {
-//   //change the current working directory
-//   process.chdir("/tmp");
-//   // Log the new current working directory after the change
-//   console.log(`The new working directory is ${process.cwd()}`);
-// } catch (error) {
-//   //Log any error that might occur during the directory change
-//   console.log(`Something happpened: ${error}`);
-// }
-
-// const sandwich = "Ham and Cheese";
-// const drink = "Lemonade";
-
-// // Named exports using the exports shortcut
-// exports.sandwich = sandwich;
-
-// // Default export using module.exports
-
-// console.log(sayHi(3));
+try {
+  //change the current working directory
+  process.chdir("/tmp");
+  // Log the new current working directory after the change
+  console.log(`The new working directory is ${process.cwd()}`);
+} catch (error) {
+  //Log any error that might occur during the directory change
+  console.log(`Something happpened: ${error}`);
+}
