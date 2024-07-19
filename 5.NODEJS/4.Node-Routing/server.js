2e38bc0f04d361e49cf3131c94c5b150dfde1277
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 14:59:46 +0530

    MERN COURSE : Node Js 27

--- 5.NODEJS/4.Node-Routing/server.js
+++ 5.NODEJS/4.Node-Routing/server.js
@@ -3,6 +3,7 @@
 //! =====================================
 
 const http = require("http");
+const url = require("url");
 
 //! =====================================
 //? =====  Define The Handler =====
@@ -10,16 +11,58 @@ const http = require("http");
 
 const requestHandler = (req, res) => {
   console.log(req.url);
-  //! send response
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.end("Hello, World!\n");
+  //! pass the url
+  const passedUrl = url.parse(req.url, true);
+  console.log(passedUrl);
+  //! Home route
+  if (passedUrl.pathname === "/" && req.method === "GET") {
+    //! send response
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(`
+      <html>
+        <body>
+          <h1>Welcome to My Simple Server!</h1>
+          <p>This is a basic server created using Node.js and HTTP module.</p>
+        </body>
+      </html>
+    `);
+    return;
+  }
+
+  //! ABOUT route
+  else if (passedUrl.pathname === "/about" && req.method === "GET") {
+    //! send response
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(`
+      <html>
+        <body>
+          <h1>Welcome to About us section</h1>
+          <p>This is a basic server created using Node.js and HTTP module.</p>
+               <p>This is About Us SECTION.</p>
+        </body>
+      </html>
+    `);
+    return;
+  } else {
+    //! send 404 response
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end(`
+      <html>
+        <body>
+          <h1>Error 404 - Page Not Found</h1>
+          <p>The requested URL was not found on this server.</p>
+        </body>
+      </html>
+    `);
+    return;
+  }
 };
 
 //! =====================================
 //? =====  Create The Server =====
 //! =====================================
 
-const server = http.createserver(requestHandler);
+const server = http.createServer(requestHandler);
 
 //! =====================================
 //? =====  Start The Server =====
