f8541e42bfea2d65ca36563dd322858d3dea6c56
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 11:53:53 +0530

    MERN COURSE : Node Js 17

--- 5.NODEJS/2.MODULES/4.OS-API/app.js
+++ 5.NODEJS/2.MODULES/4.OS-API/app.js
@@ -1,35 +1,58 @@
+//! =====================
+
 const os = require("os");
 
-// os.arch()
-//console.log("CPU Architecture", os.arch());
+//! os.arch()
+// console.log("CPU Architecture", os.arch());
 
-//os.cpus()
-//console.log("CPU info", os.cpus());
+//?-------------------------
 
-//os.endianness()
-//console.log(os.endianness());
+//!os.cpus()
+// console.log("CPU info", os.cpus());
 
-//os.freemem()
-//console.log(os.freemem());
+//?-------------------------
 
-//os.homedir()
-//console.log(os.homedir());
+//! os.endianness()
+// console.log(os.endianness());
 
-//os.hostname()
+//?-------------------------
+
+//! os.freemem()
+// console.log(os.freemem());
+
+//?-------------------------
+
+//! os.homedir()
+// console.log(os.homedir());
+
+//?-------------------------
+
+//! os.hostname()
 // console.log(os.hostname());
 
-// os.loadavg()
-//console.log(os.loadavg());
+//?-------------------------
 
-//os.networkInterfaces()
-//console.log(os.networkInterfaces());
+//! os.loadavg()
+// console.log(os.loadavg());
 
-// os.platform()
-//console.log(os.platform());
+//?-------------------------
 
-//os.release()
-//console.log(os.release());
+//! os.networkInterfaces()
+// console.log(os.networkInterfaces());
 
-//os.totalmem()
+//?-------------------------
 
-console.log(os.totalmem());
+//! os.platform()
+// console.log(os.platform());
+
+//?-------------------------
+
+//! os.release()
+// console.log(os.release());
+
+//?-------------------------
+
+//! os.totalmem()
+// console.log(os.totalmem());
+
+//! =====================
