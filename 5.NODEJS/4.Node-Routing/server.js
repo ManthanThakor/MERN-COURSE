3be018b9adf99171556d7218c2632702bec3b341
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 14:43:33 +0530

    MERN COURSE : Node Js 26

--- 5.NODEJS/4.Node-Routing/server.js
+++ 5.NODEJS/4.Node-Routing/server.js
@@ -1,12 +1,31 @@
 //! =====================================
 //? =====  IMPORTS Required modules =====
 //! =====================================
+
+const http = require("http");
+
 //! =====================================
 //? =====  Define The Handler =====
 //! =====================================
+
+const requestHandler = (req, res) => {
+  console.log(req.url);
+  //! send response
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("Hello, World!\n");
+};
+
 //! =====================================
 //? =====  Create The Server =====
 //! =====================================
+
+const server = http.createserver(requestHandler);
+
 //! =====================================
 //? =====  Start The Server =====
 //! =====================================
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`This server is running on http://localhost:${PORT}`);
+});
