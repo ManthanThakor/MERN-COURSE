603cd6548490246acc5925f45498e5b18fdf8de0
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 10:58:50 +0530

    MERN COURSE : Node Js 12

--- 5.NODEJS/2.MODULES/1.commonJS/app.js
+++ 5.NODEJS/2.MODULES/1.commonJS/app.js
@@ -1,1 +1,27 @@
 //! ========================
+const greet = require("./utills");
+const firstName = require("./utills");
+const { utills, add, subtract, sayBye, sayHi } = require("./utills");
+
+console.log("App Started...");
+// console.log(module);
+// console.log(firstName);
+// console.log(utills("Alice"));
+// console.log(greet);
+
+//? -----------------------------
+
+// console.log(utills);
+// const { add, subtract } = utills;
+// console.log(add(10, 5));
+// console.log(subtract(10, 5));
+// console.log(add(2, 6));
+// console.log(subtract(10, 5))
+
+//? -----------------------------
+
+console.log(sayHi("Alice"));
+console.log(sayBye("Bob"));
+console.log(sayHi("Charlie"));
+console.log(sayBye("David"));
+console.log(sayHi("Emily"));
--- 5.NODEJS/2.MODULES/1.commonJS/utills.js
+++ 5.NODEJS/2.MODULES/1.commonJS/utills.js
@@ -1,43 +1,56 @@
-//---ways of exporting modules in csjs---
+//! ========================
+//? ---ways of exporting modules in csjs---
+//! ========================
+
 //!Single Function Export
 //! Object export
 //! Named function export
 //! using export shorthand
 
-// //?Single Function Export
+//! ========================
+//? Single Function Export
+//! ========================
 
-// const firstname = "Alice";
-//Expose this file
+const firstName = "Alice";
+// export this file
+//? module.exports = firstName;
 
-// const greet = (name) => {
-//   return `Hello ${name}`;
-// };
-// module.exports = greet;
+const greet = (name) => {
+  return `Hello ${name}`;
+};
+//? module.exports = greet;
 
+//! ========================
 //?---Object export---
+//! ========================
 
 function add(a, b) {
   return a + b;
 }
-function subtract(a, b) {
+
+const subtract = (a, b) => {
   return a - b;
-}
+};
 
-// module.exports = {
-//   subtract,
-//   add,
-// };
+//? module.exports = {
+// ?  subtract,
+//?   add,
+//? };
 
-//! Named function export
-// module.exports.sayHi = (name) => {
-//   return `Hi ${name}`;
-// };
+//! ========================
+//? Named function export
+//! ========================
 
-// module.exports.sayBye = function (name) {
-//   return `Goodbye ${name}`;
-// };
+//? module.exports.sayHi = (name) => {
+//?   return `Hi ${name}`;
+//? };
+
+//? module.exports.sayBye = function (name) {
+//?   return `Goodbye ${name}`;
+//? };
 
 //! using export shorthand
+
 exports.sayHi = (name) => {
   return `Hi ${name}`;
 };
