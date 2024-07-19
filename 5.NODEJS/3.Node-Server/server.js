de8fa893aad7a9cc80cdfa6197b38c1ea4b0ca9d
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 14:29:01 +0530

    MERN COURSE : Node Js 22

--- 5.NODEJS/3.Node-Server/server.js
+++ 5.NODEJS/3.Node-Server/server.js
@@ -1,3 +1,31 @@
-//! ================================
-//? =========  IMPORTS  =========
-//! ================================
+//! =====================================
+//? =====  IMPORTS Required modules =====
+//! =====================================
+
+const http = require("http");
+// console.log(http);
+
+//! =====================================
+//? =====  Define The Handler =====
+//! =====================================
+
+const requestHandler = (request, response) => {
+  //! send response
+  res.writehead(200, { "content-type": "text/plain" });
+  res.end("Hello from Node.js Server\n");
+};
+
+//! =====================================
+//? =====  Create The Server =====
+//! =====================================
+
+const server = http.createServer(requestHandler);
+
+//! =====================================
+//? =====  Start The Server =====
+//! =====================================
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
