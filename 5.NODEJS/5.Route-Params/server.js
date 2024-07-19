f0d3fa12220bded129814bcf10a711ff340d7a18
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-19 15:38:06 +0530

    MERN COURSE : Node Js 29

--- 5.NODEJS/5.Route-Params/server.js
+++ 5.NODEJS/5.Route-Params/server.js
@@ -9,10 +9,39 @@ const url = require("url");
 //? =====  Define The Handler =====
 //! =====================================
 
+const requestHandler = (req, res) => {
+  //! pass the url
+  const passedURL = url.parse(req.url, true);
+  const pathName = passedURL.pathname;
+  console.log(pathName);
+  //! split the pathName
+  const pathComponent = pathName.split("/").filter(Boolean);
+  console.log(pathComponent);
+  if (pathComponent[0] === "products" && pathComponent[1]) {
+    // Take the Product and send to the user
+    //perform db query
+    const ProductId = pathComponent[1];
+    //! Send the response
+    res.writeHead(200, { "Content-Type": "text/pai n" });
+    res.end(`Product ID ${ProductId}`);
+  } else {
+    // Send a 404 not found response
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+};
+
 //! =====================================
 //? =====  Create The Server =====
 //! =====================================
 
+const server = http.createServer(requestHandler);
+
 //! =====================================
 //? =====  Start The Server =====
 //! =====================================
+
+const PORT = "3000";
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/`);
+});
