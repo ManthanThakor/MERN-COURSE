2c252f435481126c3034b26fabd2d1b657c5c495
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-23 14:13:22 +0530

    MERN COURSE : Express JS 43

--- 6.EXPRESS-JS/10.EJS-Layouts/server.js
+++ 6.EXPRESS-JS/10.EJS-Layouts/server.js
@@ -1,43 +1,59 @@
 const express = require("express");
-const ejsLayout = require("express-ejs-layouts");
 const path = require("path");
+const ejsMate = require("ejs-mate"); // Import ejs-mate
 const app = express();
-const PORT = 8082;
-//Serve the static files/folder
+
+//! =================================
+//? === SERVE THE STATIC FILE ===
+//! =================================
+
 app.use(express.static(path.join(__dirname, "public")));
-//Set the view engine as  ejs
+
+//! Set the view engine
 app.set("view engine", "ejs");
-//Plugin the ejs layout as a middleware
-app.use(ejsLayout);
+
+//! Set the views directory (if it's not in the default location)
+app.set("views", path.join(__dirname, "views"));
+
+//! Use ejs-mate for all .ejs files
+app.engine("ejs", ejsMate); // Use ejs-mate as the layout engine
+
+//! Create PORT
+const PORT = process.env.PORT || 8082;
+
+// Set default layout
 app.set("layout", "layout/main-layout");
 
-//Render Home page/route
+// Render Home page/route
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-//Render About page/route
+// Render About page/route
 app.get("/about", (req, res) => {
   res.render("about");
 });
-//Render Contact page/route
+
+// Render Contact page/route
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
-//Render Gallery page/route
+
+// Render Gallery page/route
 app.get("/gallery", (req, res) => {
   res.render("gallery");
 });
 
-//404 Error handler
+// 404 Error handler
 app.use((req, res, next) => {
   const error = new Error("Page Not Found");
-
   next(error);
 });
+
 app.use((err, req, res, next) => {
   console.log(err.message);
   res.render("error", { error: err.message });
 });
-//!Start the server
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
+//! Start the server
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
