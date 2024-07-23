b06df75fcb590c76036b29b5e1e4bfddcaf166b6
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-23 12:01:19 +0530

    MERN COURSE : Express JS 39

--- 6.EXPRESS-JS/8.rendering-assets-ejs/server.js
+++ 6.EXPRESS-JS/8.rendering-assets-ejs/server.js
@@ -45,6 +45,46 @@ app.get("/gallery", (req, res) => {
   res.render("gallery");
 });
 
+// Render UserData page/Route
+app.get("/user", (req, res) => {
+  //! Dummy User Content
+  const userData = {
+    username: "Manthan",
+    age: 21,
+    isPremiumUser: true,
+    email: "Manthu@gmail.com",
+    isLogin: true,
+  };
+  res.render("userData", { userData });
+});
+
+// Render Product page/Route
+
+app.get("/products", (req, res) => {
+  const products = [
+    {
+      name: "Laptop",
+      price: 999,
+      image: "laptop.jpg",
+      description: "A high-performance laptop with 16GB RAM and 512GB SSD.",
+    },
+    {
+      name: "Phone",
+      price: 799,
+      image: "phone.jpg",
+      description:
+        "A smartphone with a stunning display and excellent camera quality.",
+    },
+    {
+      name: "TV",
+      price: 1200,
+      image: "tv.jpg",
+      description: "A 55-inch smart TV with 4K resolution and HDR support.",
+    },
+  ];
+  res.render("products", { products });
+});
+
 //! =================================
 //? === start the server ===
 //! =================================
