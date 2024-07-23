d2ae5c0c54abd6e7186b99c0a891938d3968de55
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-23 14:34:20 +0530

    MERN COURSE : Express JS 44

--- 6.EXPRESS-JS/10.EJS-Layouts/server.js
+++ 6.EXPRESS-JS/10.EJS-Layouts/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const ejsMate = require("ejs-mate"); // Import ejs-mate
+const ejsLayout = require("express-ejs-layouts");
 const app = express();
 
 //! =================================
@@ -15,14 +15,16 @@ app.set("view engine", "ejs");
 //! Set the views directory (if it's not in the default location)
 app.set("views", path.join(__dirname, "views"));
 
-//! Use ejs-mate for all .ejs files
-app.engine("ejs", ejsMate); // Use ejs-mate as the layout engine
+//! Plug in the ejs layout as a middleware
+app.use(ejsLayout);
+
+//! Set the default layout for all views
+app.set("layout", "layouts/main-layout");
+
+//? === ROUTING ===
 
 //! Create PORT
 const PORT = process.env.PORT || 8082;
-
-// Set default layout
-app.set("layout", "layout/main-layout");
 
 // Render Home page/route
 app.get("/", (req, res) => {
@@ -42,17 +44,6 @@ app.get("/contact", (req, res) => {
 // Render Gallery page/route
 app.get("/gallery", (req, res) => {
   res.render("gallery");
-});
-
-// 404 Error handler
-app.use((req, res, next) => {
-  const error = new Error("Page Not Found");
-  next(error);
-});
-
-app.use((err, req, res, next) => {
-  console.log(err.message);
-  res.render("error", { error: err.message });
 });
 
 //! Start the server
