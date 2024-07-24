013d5dc7cdea35df87ce3e47e5e8943b474f0e38
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-24 13:30:44 +0530

    MERN COURSE : MONGODB 8

--- 7.MONGODB/2.CRUD-Operations/server.js
+++ 7.MONGODB/2.CRUD-Operations/server.js
@@ -22,13 +22,16 @@ const PORT = process.env.PORT || 5000;
 
 // 1. Create The Client
 
-const Client = new MongoClient("YOUR MONGODB URL", {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const Client = new MongoClient(
+  "mongodb+srv://thakormanthan849:HOQnOxugSZFFXWMG@myfirstmongodb.jm4tch7.mongodb.net/Student-database",
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
 
 // 2. Function to connect
 
@@ -36,6 +39,56 @@ const connectDB = async () => {
   try {
     await Client.connect();
     console.log("MongoDB Connected Successfully");
+
+    //!------------
+    //? 1. Database name (school)
+    //!------------
+
+    const database = Client.db("Student-database");
+
+    //!------------
+    //? 2. Collections (Student)
+    //!------------
+
+    const Student = database.collection("student");
+
+    //! =====================================
+    //? =======CRUD OPERATIONS =========
+    //? ======= Write Operation =========
+    //! =====================================
+
+    //!------------
+    //? 4. Document() using the InsertOne
+    //!------------
+
+    const result = await Student.insertOne({
+      name: "Manthan",
+      age: 21,
+      subjects: ["Math", "Science"],
+    });
+    console.log(result);
+    console.log("Document inserted successfully");
+
+    //!------------
+    //? 5. Document() using the InsertMany
+    //!------------
+
+    const results = await Student.insertMany([
+      {
+        name: "Devil",
+        age: 22,
+        pass: false,
+        subjects: ["English", "History"],
+      },
+      {
+        name: "DRONE",
+        age: 20,
+        pass: true,
+        subjects: ["Math", "Science"],
+      },
+    ]);
+    console.log(results);
+    console.log("Many Documents inserted successfully");
   } catch (error) {
     console.error(error);
   }
