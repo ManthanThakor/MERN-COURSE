e586910fa44f7a40b1d6c181a153703c2c610d94
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-24 12:03:14 +0530

    MERN COURSE : MONGODB 3

--- 7.MONGODB/1.Getting-started/server.js
+++ 7.MONGODB/1.Getting-started/server.js
@@ -22,19 +22,24 @@ const PORT = process.env.PORT || 5000;
 
 // 1. Create The Client
 
-const Client = new MongoClient("some mongodb url ", {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const Client = new MongoClient(
+  "mongodb+srv://thakormanthan849:HOQnOxugSZFFXWMG@myfirstmongodb.jm4tch7.mongodb.net/Student-database",
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
+// thakormanthan849
+// HOQnOxugSZFFXWMG
 
 // 2. Function to connect
 
 const connectDB = async () => {
   try {
-    await client.connect();
+    await Client.connect();
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error(error);
