8ce8dcd3ca977d3b7f13681b96870dc3817d8816
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-26 13:50:23 +0530

    MERN COURSE : MONGOOSE 22

--- 8.MONGOOSE/10.Data-Modelling-one-many/server.js
+++ 8.MONGOOSE/10.Data-Modelling-one-many/server.js
@@ -1,83 +1,83 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 const app = express();
-const PORT = 8082;
+const PORT = process.env.PORT || 5000;
 
-// const mongodbURL =
-//   "mongodb+srv://twentekghana:xWzu0Yn69lU7yU9K@mongodb-basics.8pldozv.mongodb.net/?retryWrites=true&w=majority";
-const mongodbURL = "mongodb://localhost:27017/masynctech";
+const dbUrl =
+  "mongodb+srv://thakormanthan849:HOQnOxugSZFFXWMG@myfirstmongodb.jm4tch7.mongodb.net/one-to-many";
 
-//! 1. Connect to mongodb using mongoose
+// Connect to MongoDB
 const connectToDB = async () => {
   try {
-    await mongoose.connect(mongodbURL);
-    console.log("Mongodb has been connected successfully");
+    await mongoose.connect(dbUrl, { autoIndex: true });
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.log(`Error connecting to mongodb ${error}`);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
   }
 };
+
 connectToDB();
-// ! ----one-many relationship-----
-//commentSchema
-const commentSchema = new mongoose.Schema(
-  {
-    text: String,
-  },
-  {
-    timestamps: true,
-  }
-);
-const Comment = mongoose.model("Comment", commentSchema);
-//blogPostSchema
-const blogPostSchema = new mongoose.Schema(
-  {
-    title: String,
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-  },
-  {
-    timestamps: true,
-  }
-);
 
-//!Models
-const Post = mongoose.model("Post", blogPostSchema);
-//!-----Create Post---
-// const createPost = async () => {
-//   try {
-//     const newPost = await Post.create({ title: "Awesome Fullstack course" });
-//     console.log(newPost);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// createPost();
-//!-----Create comment---
-// const createComment = async () => {
-//   try {
-//     //!Find the post
-//     const postFound = await Post.findById("6533affd2265bf3f587c4226");
-//     //Create the comment
-//     const newComment = await Comment.create({ text: "Awesome post2" });
-//     //Pust the comment into the post
-//     postFound.comments.push(newComment);
-//     //! Resave the post
-//     await postFound.save();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// createComment();
-//!-----Create comment---
-const fetchPosts = async () => {
+// Define Comment schema
+const CommentSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+// Define BlogPost schema
+const blogPostSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+});
+
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+// Create a new blog post
+const createBlogPost = async () => {
   try {
-    //!Find the post
-    const posts = await Post.find().populate("comments");
-    console.log(posts[0].comments);
+    const newBlogPost = new BlogPost({
+      title: "My First Blog Post",
+      content: "This is my first blog post",
+    });
+
+    const result = await newBlogPost.save();
+    console.log("Blog post created", result);
   } catch (error) {
-    console.log(error);
+    console.error("Error creating blog post", error.message);
   }
 };
-fetchPosts();
 
-//Start the server
-app.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
+createBlogPost();
+
+// Create a new comment and add it to a blog post
+const createComment = async () => {
+  try {
+    const newComment = new Comment({
+      text: "This is a sample comment",
+    });
+
+    const result = await newComment.save();
+
+    const blogPost = await BlogPost.findById("66a351573f21ea74d2c2584e");
+    if (blogPost) {
+      blogPost.comments.push(result._id);
+      await blogPost.save();
+      console.log("Comment created and added to blog post", result);
+    } else {
+      console.error("Blog post not found");
+    }
+  } catch (error) {
+    console.error("Error creating comment", error.message);
+  }
+};
+
+createComment();
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
