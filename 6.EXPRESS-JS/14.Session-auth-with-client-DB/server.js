ce6b9f101c15d2f1d155098778ecc70c9f043d35
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-27 17:34:04 +0530

    MERN COURSE : Express Authentication 16

--- 6.EXPRESS-JS/14.Session-auth-with-client-DB/server.js
+++ 6.EXPRESS-JS/14.Session-auth-with-client-DB/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-
 const bcrypt = require("bcryptjs");
 const app = express();
 
