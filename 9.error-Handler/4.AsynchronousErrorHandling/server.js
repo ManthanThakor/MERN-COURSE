f501bcd0bfb720c73cc9b5634d31ae22ecce0ece
Manthan Thakor <thakormanthan849@gmail.com>
2024-07-29 11:46:24 +0530

    MERN COURSE : error handler 5

--- 9.error-Handler/4.AsynchronousErrorHandling/server.js
+++ 9.error-Handler/4.AsynchronousErrorHandling/server.js
@@ -3,6 +3,7 @@
 //! =================================
 
 const express = require("express");
+const axios = require("axios");
 
 //! =================================
 //? === INSTANCE ===
@@ -14,15 +15,16 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-//! Simulate an In the Middlewares
-app.use((req, res, next) => {
-  //! Simulate an error condition
-  const isError = true;
-  if (isError) {
-    const err = new Error("Something went wrong");
-    next(err);
-  } else {
-    next();
+//! Route to fetch posts
+
+app.get("/posts", async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    res.json(response.data);
+  } catch (error) {
+    next(error);
   }
 });
 
@@ -35,14 +37,21 @@ app.get("/", (req, res) => {
 //! custom Error Handling Middleware
 
 app.use((err, req, res, next) => {
-  //setting HTTP status code
-  console.error(err.stack);
-  res.status(err.status || 500);
-  res.json({
-    error: err.message,
-    status: err.status,
-    stack: err.stack,
-  });
+  // check if its an axios error
+  if (err.isAxiosError) {
+    console.error("Axios error:", err.message);
+    if (err.response) {
+      res.status(err.response.status).json(err.response.data);
+    } else {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
+    console.error(`Server error: ${err.message}`);
+    res.status(503).json({
+      error: "Service Unavailable",
+      message: err.message,
+    });
+  }
 });
 
 //! =================================
