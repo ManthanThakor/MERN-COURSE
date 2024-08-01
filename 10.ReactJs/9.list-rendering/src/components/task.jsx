c2955d6970fd47891dc1f59168e011fc3c14487f
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-01 11:26:33 +0530

    MERN COURSE : REACT 31

---
+++ 10.ReactJs/9.list-rendering/src/components/task.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const task = () => {
+  return (
+    <>
+      <div className="task-main">
+        <div className="task-title">
+          <h1>Task Title</h1>
+        </div>
+        <div className="task-description">
+          <p>Task Description</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default task;
