3d85eccebd512035c5d83e71fcc9f875f366bf78
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-01 11:32:59 +0530

    MERN COURSE : REACT 33

--- 10.ReactJs/9.list-rendering/src/components/Tasks.jsx
+++ 10.ReactJs/9.list-rendering/src/components/Tasks.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, name: "Finish React tutorial", completed: true },
+    { id: 2, name: "Go grocery shopping", completed: false },
+    { id: 3, name: "Prepre dinner", completed: false },
+    { id: 4, name: "Read a book", completed: false },
+  ]);
   return (
     <>
       <div className="task-main">
