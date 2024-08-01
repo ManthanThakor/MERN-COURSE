4ac1f611707b7266a09c565cddc99a1661e00446
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-01 10:21:10 +0530

    MERN COURSE : REACT 24

--- 10.ReactJs/7.components-practice/src/App.jsx
+++ 10.ReactJs/7.components-practice/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="instagram-post-1">
+        <div className="akar-iconstwitter-fill" />
+
+        <section className="post-frame">
+          <div className="post-frame-child" />
+          <div className="rectangle-shape-parent">
+            <div className="rectangle-shape" />
+            <div className="wrapper-ellipse-shape">
+              {/* Image component */}
+              <ProfileImage image="https://cdn.pixabay.com/photo/2024/01/10/16/20/woman-8499928_1280.jpg" />
+              a
+            </div>
+          </div>
+          <div className="secondary-frame">
+            <div className="title-text-parent">
+              <div className="title-text">
+                {/* profile name component */}
+                <ProfileName name="Masynctech" />
+                {/* profile role */}
+                <ProfileRole role="MERN Stack Dev" />
+              </div>
+              <div className="personal-choice">
+                <div className="vertical-line" />
+                {/* About */}
+                <ProfileAbout />
+              </div>
+            </div>
+            <div className="brand-frame">
+              {/* Social icons */}
+              <Profileicons />
+            </div>
+          </div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
