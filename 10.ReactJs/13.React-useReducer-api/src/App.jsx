ff6581b25b87481c410ba083c1e48e03ec16d532
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-02 13:20:31 +0530

    MERN COURSE : REACT 46

--- 10.ReactJs/13.React-useReducer-api/src/App.jsx
+++ 10.ReactJs/13.React-useReducer-api/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useReducer } from "react";
 
+import "./App.css";
+// import Counter from "./components/Counter";
+
+const initialState = {
+  count: 0,
+};
+
+//! Define the reducer function to handle state transitions
+
+const reducer = (state, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  } else if (action.type === "DECREMENT") {
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  } else {
+    return state;
+  }
+};
+const Counterr = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div className="counter-container">
+      <h1>{state.count}</h1>
+      <button
+        className="button"
+        onClick={() => dispatch({ type: "INCREMENT" })}
+      >
+        +
+      </button>
+      <button
+        className="button"
+        onClick={() => dispatch({ type: "DECREMENT" })}
+      >
+        -
+      </button>
+    </div>
+  );
+};
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="app-container">
+        <h1 className="title">React Counter</h1>
+        <p className="sub-description">
+          A simple counter application using Context API
         </p>
+        <Counterr />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 10.ReactJs/13.React-useReducer-api/src/components/Counter.jsx
@@ -0,0 +1,16 @@
+// Counter.js
+import React from "react";
+
+import "../App.css";
+
+const Counter = () => {
+  return (
+    <div className="counter-container">
+      <h1>20</h1>
+      <button className="button">+</button>
+      <button className="button">-</button>
+    </div>
+  );
+};
+
+export default Counter;
