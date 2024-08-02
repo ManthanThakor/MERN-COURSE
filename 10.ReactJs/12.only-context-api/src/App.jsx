e67175d5b55832b64e6ba7bbb0616e2bc40f5f19
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-02 11:44:55 +0530

    MERN COURSE : REACT 44

--- 10.ReactJs/12.only-context-api/src/App.jsx
+++ 10.ReactJs/12.only-context-api/src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState, createContext, useContext } from "react";
+//Create a context to hold the count state and updater functions
+const CountContext = createContext();
 
 const App = () => {
   return (
