6650845d02293f903c7651dd6d0930736f3e898b
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-02 15:16:12 +0530

    MERN COURSE : REACT 48

---
+++ 10.ReactJs/15.react-router/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="App">
+        <h1 style={{ textAlign: "center" }}>Main App</h1>
+        <h2>Count: {count}</h2>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 10.ReactJs/IMPORTANT.md
@@ -0,0 +1,2 @@
+For state management in react : https://chatgpt.com/share/772007bf-0ef4-4d2b-aa58-39587a7fa11f
+For React router dom in react : https://chatgpt.com/share/88d2084a-6629-4f4f-902f-9e0f2a68d3ae
