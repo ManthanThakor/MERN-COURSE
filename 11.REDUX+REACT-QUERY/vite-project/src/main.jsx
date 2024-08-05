2a231cbe13c5120dcb3e42fafd205d9979d47f14
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-05 13:53:36 +0530

    MERN COURSE : REACT 78

--- 11.REDUX+REACT-QUERY/vite-project/src/main.jsx
+++ 11.REDUX+REACT-QUERY/vite-project/src/main.jsx
@@ -4,11 +4,19 @@ import App from "./App.jsx";
 import "./index.css";
 import store from "./redux/store/store.js";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+//! instance of react query
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 );
