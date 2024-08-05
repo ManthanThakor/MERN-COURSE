6a80e31ee229bfcbed3d8963ed0a836054cfd9bb
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-05 10:17:50 +0530

    MERN COURSE : REACT 64

--- 11.REDUX+REACT-QUERY/Authentication-API/middlewares/isAuth.js
+++ 11.REDUX+REACT-QUERY/Authentication-API/middlewares/isAuth.js
@@ -1,30 +1,31 @@
 const jwt = require("jsonwebtoken");
 
 const isAuthenticated = (req, res, next) => {
+  // Get the token from the header
   const authHeader = req.headers.authorization;
+  console.log("Authorization Header:", authHeader);
 
   if (!authHeader) {
     return res.status(401).json({ message: "Authorization header missing" });
   }
 
   const token = authHeader.split(" ")[1];
+  console.log("Token:", token);
 
   if (!token) {
     return res.status(401).json({ message: "Token missing" });
   }
 
-  jwt.verify(
-    token,
-    process.env.JWT_SECRET || "your-secret-key",
-    (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: "Token invalid or expired" });
-      }
-
-      req.user = decoded.id;
-      next();
+  // Verify token
+  jwt.verify(token, "anyKey", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Token invalid or expired" });
     }
-  );
+
+    // Save the user into req.user
+    req.user = decoded.id;
+    next();
+  });
 };
 
 module.exports = isAuthenticated;
