af0d788618e3d805533c04ea8fb8acc52e70e50e
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-05 10:40:41 +0530

    MERN COURSE : REACT 69

--- 11.REDUX+REACT-QUERY/vite-project/src/App.jsx
+++ 11.REDUX+REACT-QUERY/vite-project/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+      </div>
     </>
   );
 }
