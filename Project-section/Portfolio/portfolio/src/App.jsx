c97cccbceafb8d581c40b3a6f866eeb2e602fa5a
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-06 11:26:37 +0530

    MERN COURSE : project 1 : 4

--- Project-section/Portfolio/portfolio/src/App.jsx
+++ Project-section/Portfolio/portfolio/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Components/Header/Header";
+import Services from "./Components/Services/Services";
+import Works from "./Components/Works/Works";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
